40b2826a2e12bc5b650aaad3dc5b1342e54b7aba
RTB13 <rtberrigan@gmail.com>
2025-02-26 15:38:25 -0500

    Cat and Dog

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class AnimalPicker {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Which animal would you like to see? (dog/cat)");
+        String choice = scanner.nextLine().toLowerCase();
+        
+        if (choice.equals("dog")) {
+            System.out.println("DOG ASCII ART HERE");
+        } else if (choice.equals("cat")) {
+            System.out.println("CAT ASCII ART HERE");
+        }         
+        scanner.close();
+    }
 }
